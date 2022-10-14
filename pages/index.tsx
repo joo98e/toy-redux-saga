beda7ccc237d98089ff342f7822b08c7753cf5f2
Ayaan <ayaan@myfair.co>
2022-10-14 18:57:30 +0900

    refactor : checkmark

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,59 @@ import { useDispatch } from "react-redux";
 import { getArticleRequest } from "@store/slices/section/slice";
 import NewCard from "@components/test/NewCard";
 
-const Container = styled.div``;
+const Container = styled.div`
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  .checkmark__circle {
+    stroke-dasharray: 166;
+    stroke-dashoffset: 166;
+    stroke-width: 2;
+    stroke-miterlimit: 10;
+    stroke: #7ac142;
+    fill: none;
+    animation: stroke 0.6s cubic-bezier(0.65, 0, 0.45, 1) forwards;
+  }
+  .checkmark {
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    display: block;
+    stroke-width: 2;
+    stroke: #fff;
+    stroke-miterlimit: 10;
+    margin: 10% auto;
+    box-shadow: inset 0px 0px 0px #7ac142;
+    animation: fill 0.6s ease-in-out 0.4s forwards,
+      scale 0.3s ease-in-out 0.9s both;
+  }
+  .checkmark__check {
+    transform-origin: 50% 50%;
+    stroke-dasharray: 48;
+    stroke-dashoffset: 48;
+    animation: stroke 0.3s cubic-bezier(0.65, 0, 0.45, 1) 0.8s forwards;
+  }
+  @keyframes stroke {
+    100% {
+      stroke-dashoffset: 0;
+    }
+  }
+  @keyframes scale {
+    0%,
+    100% {
+      transform: none;
+    }
+    50% {
+      transform: scale3d(1.4, 1.4, 1);
+    }
+  }
+  @keyframes fill {
+    100% {
+      box-shadow: inset 0px 0px 0px 30px #7ac142;
+    }
+  }
+`;
 
 const Home: NextPage = () => {
   const dispatch = useDispatch();
@@ -17,6 +69,25 @@ const Home: NextPage = () => {
     <Container>
       <NewCard></NewCard>
       <button onClick={handleClickGetData}>213</button>
+
+      <svg
+        className="checkmark"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 52 52"
+      >
+        <circle
+          className="checkmark__circle"
+          cx="26"
+          cy="26"
+          r="25"
+          fill="none"
+        />
+        <path
+          className="checkmark__check"
+          fill="none"
+          d="M14.1 27.2l7.1 7.2 16.7-16.8"
+        />
+      </svg>
     </Container>
   );
 };
